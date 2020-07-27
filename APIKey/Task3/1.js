037f3e58987a3431e265c7fdb8b3fdde14060fe9
Revathi-Andiappan <65998783+Revathi-Andiappan@users.noreply.github.com>
2020-07-27 12:05:00 +0530

    Update 1.js

--- APIKey/Task3/1.js
+++ APIKey/Task3/1.js
@@ -1,6 +1,5 @@
-
 var request = new XMLHttpRequest()
-var url_string = 'https://dog.ceo/api/breeds/list/all';
+var url_string = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3efd2ed5f377b802a22794e61c47d194';
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET',url_string , true)
 request.send();
